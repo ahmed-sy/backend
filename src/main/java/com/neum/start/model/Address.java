2ad1068e9ce8359f47da04810010ac5a12094dcd
Amandos <atmda.sy@gmail.com>
2024-03-21 08:17:01 +0100

    init

---
+++ src/main/java/com/neum/start/model/Address.java
@@ -0,0 +1,54 @@
+package com.neum.start.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="address")
+public class Address {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    private	Long id;	
+	
+	 @ManyToOne
+	 @JoinColumn(name="user_id")
+	 User user;
+	
+	@Column(name="street")
+    private	String street;
+	
+	
+	@Column(name="haus_number")
+    private	String haus_number;
+	
+	@Column(name="city")
+    private	String city;
+
+	
+	@Column(name="country")
+	private String country;
+	
+	@Column(name="country_code")
+	private String countryCode;
+	
+	@Column(name="plz")
+	private String plz;
+    
+
+}
